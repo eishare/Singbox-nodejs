4d254d7898e3a1a9c4c7c236d9b8dbdc992378fb
EasyShare <easonhunt@protonmail.com>
2026-09-02 18:42:40 +0800

    Update start.sh

--- Argo Tunnel/start.sh
+++ Argo Tunnel/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
 export MALLOC_ARENA_MAX=2
-exec node --max-old-space-size=48 index.js
+
+export NODE_OPTIONS="--optimize-for-size --gc-interval=100"
+
+exec node --max-old-space-size=80 index.js

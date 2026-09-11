49db3dd32d8463b5eb8037c515260ecc5af596cf
EasyShare <easonhunt@protonmail.com>
2026-09-11 18:18:06 +0800

    Update start.sh

--- Argo Tunnel/start.sh
+++ Argo Tunnel/start.sh
@@ -21,42 +21,33 @@ if [ -z "$TOTAL_MEM_MB" ] || [ "$TOTAL_MEM_MB" -eq 0 ] || [ "$TOTAL_MEM_MB" -gt 
 fi
 
 if [ -z "$TOTAL_MEM_MB" ] || [ "$TOTAL_MEM_MB" -eq 0 ]; then
-    TOTAL_MEM_MB=128
+    TOTAL_MEM_MB=100
 fi
-
-export MALLOC_ARENA_MAX=2
-
-export GOMAXPROCS=2
 
 export GODEBUG="madvdontneed=1"
 
 if [ "$TOTAL_MEM_MB" -le 160 ]; then
-
     export MALLOC_ARENA_MAX=1
-    export GOMAXPROCS=1
-    export NODE_OPTIONS="--max-old-space-size=30 --optimize-for-size"
+    export NODE_OPTIONS="--max-old-space-size=24 --optimize-for-size --gc-interval=100"
 
 elif [ "$TOTAL_MEM_MB" -lt 256 ]; then
-
-    export NODE_OPTIONS="--max-old-space-size=80"
+    export MALLOC_ARENA_MAX=2
+    export NODE_OPTIONS="--max-old-space-size=40 --optimize-for-size"
 
 elif [ "$TOTAL_MEM_MB" -lt 320 ]; then
-
-    export NODE_OPTIONS="--max-old-space-size=128"
+    export MALLOC_ARENA_MAX=2
+    export NODE_OPTIONS="--max-old-space-size=50"
 
 elif [ "$TOTAL_MEM_MB" -lt 448 ]; then
-
-    export NODE_OPTIONS="--max-old-space-size=160"
+    export NODE_OPTIONS="--max-old-space-size=64"
 
 elif [ "$TOTAL_MEM_MB" -lt 576 ]; then
-
-    export NODE_OPTIONS="--max-old-space-size=200"
+    export NODE_OPTIONS="--max-old-space-size=80"
 
 else
-
-    export NODE_OPTIONS="--max-old-space-size=256"
+    export NODE_OPTIONS="--max-old-space-size=128"
 fi
 
-echo "[启动脚本] 识别到内存上限: ${TOTAL_MEM_MB}MB | NODE_OPTIONS: $NODE_OPTIONS | MALLOC_ARENA_MAX: $MALLOC_ARENA_MAX | GOMAXPROCS: $GOMAXPROCS"
+echo "[start.sh] TOTAL_MEM_MB: ${TOTAL_MEM_MB}MB | NODE_OPTIONS: $NODE_OPTIONS | MALLOC_ARENA_MAX: ${MALLOC_ARENA_MAX:-default}"
 
 exec node index.js

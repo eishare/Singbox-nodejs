e7574f97652723350a8f880bafcd88a86142a816
EasyShare <easonhunt@protonmail.com>
2026-09-26 12:34:27 +0800

    Update index.js

--- Argo+TUIC/index.js
+++ Argo+TUIC/index.js
@@ -47,13 +47,6 @@ function getGcInterval(totalMB) {
 
 const gcIntervalMs = getGcInterval(TOTAL_MEM_MB);
 
-if (typeof global.gc === "function") {
-  setInterval(() => {
-    try {
-      global.gc();
-    } catch (e) {}
-  }, gcIntervalMs);
-}
 const iataMap = {
   HKG: "香港", TPE: "台湾", NRT: "日本", HND: "日本", KIX: "日本",
   ICN: "韩国", SIN: "新加坡", BKK: "泰国", MNL: "菲律宾", SGN: "越南",
